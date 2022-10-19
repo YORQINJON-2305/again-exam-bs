aeb44e788051965d9be8cfae09a413110845154e
YORQINJON-2305 <jakbaraliyevyorqinjon505@gmail.com>
2022-10-19 12:02:52 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+
+$(function () {
+    $('.carousel-item-wrap').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: "<span><img src='./images/arrow-left.svg' alt='Left arrow'></span>",
+        nextArrow: "<span><img src='./images/arrow-right.svg' alt='Right arrow'></span>",
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    centerMode: true,
+                    centerPadding: '0',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    centerMode: true,
+                    centerPadding: '0',
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    centerMode: true,
+                    centerPadding: '0',
+                    slidesToShow:1,
+                }
+            }
+        ]
+      });
+});
