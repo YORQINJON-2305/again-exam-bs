f56ce58ccb016fa99325de6cd0fc83d18dec6d08
YORQINJON-2305 <jakbaraliyevyorqinjon505@gmail.com>
2022-10-20 19:29:39 +0500

    Site is over

--- js/main.js
+++ js/main.js
@@ -18,15 +18,12 @@ elWatchCloseBtn.addEventListener("click", function (){
 })
 
 
-
 $(function () {
     $('.customer-list').slick({
-        infinite: true,
         slidesToShow: 3,
-        lazyLoad: 'ondemand',
         slidesToScroll: 1,
-        prevArrow: "<span><img src='../images/left-arrow.svg' alt='Left arrow'></span>",
-        nextArrow: "<span><img src='../images/right-arrow.svg' alt='Right arrow'></span>",
+        prevArrow: "<div><span><svg width=\"17\" height=\"17\" viewBox=\"0 0 17 17\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n" + "<path d=\"M16.1614 7.60133H4.47533L9.84307 2.23359L8.47953 0.879654L0.797607 8.56157L8.47953 16.2435L9.83347 14.8896L4.47533 9.52182H16.1614V7.60133Z\" fill=\"currentColor\"/>\n" + "</svg></span></div>",
+        nextArrow: "<div><span><svg width=\"16\" height=\"17\" viewBox=\"0 0 16 17\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n" + "<path d=\"M7.73514 0.879623L6.38121 2.23356L11.7393 7.6013H0.0532227V9.52178H11.7393L6.38121 14.8895L7.73514 16.2435L15.4171 8.56154L7.73514 0.879623Z\" fill=\"currentColor\"/>\n" + "</svg>\n</span></div>",
         autoplay: true,
         dots: true,
         autoplaySpeed: 3000,
@@ -48,15 +45,90 @@ $(function () {
                 }
             },
             {
-                breakpoint: 640,
+                breakpoint: 768,
                 settings: {
                     centerMode: true,
                     centerPadding: '0',
                     slidesToShow: 1,
                     prevArrow: false,
-                    nextArrow: false,
+                    nextArrow: false
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    centerMode: true,
+                    centerPadding: '0',
+                    slidesToShow:1,
+                    prevArrow: false,
+                    nextArrow: false
                 }
             }
         ]
-      });
+    });
 });
+
+
+
+// $(function () {
+//     $('.customer-list').slick({
+//         infinite: true,
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         prevArrow: "<div><span><svg width=\"17\" height=\"17\" viewBox=\"0 0 17 17\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+//             "<path d=\"M16.1614 7.60133H4.47533L9.84307 2.23359L8.47953 0.879654L0.797607 8.56157L8.47953 16.2435L9.83347 14.8896L4.47533 9.52182H16.1614V7.60133Z\" fill=\"currentColor\"/>\n" +
+//             "</svg></span></div>",
+//         nextArrow: "<div><span><svg width=\"16\" height=\"17\" viewBox=\"0 0 16 17\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+//             "<path d=\"M7.73514 0.879623L6.38121 2.23356L11.7393 7.6013H0.0532227V9.52178H11.7393L6.38121 14.8895L7.73514 16.2435L15.4171 8.56154L7.73514 0.879623Z\" fill=\"currentColor\"/>\n" +
+//             "</svg>\n</span></div>",
+//         autoplay: true,
+//         dots: true,
+//         autoplaySpeed: 3000,
+//         responsive: [
+//             {
+//                 breakpoint: 1200,
+//                 settings: {
+//                     centerMode: true,
+//                     centerPadding: '0',
+//                     slidesToShow: 3
+//                 }
+//             },
+//             {
+//                 breakpoint: 992,
+//                 settings: {
+//                     centerMode: true,
+//                     centerPadding: '0',
+//                     slidesToShow: 2
+//                 }
+//             },
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                     prevArrow: false,
+//                     nextArrow: false,
+//                     slidesToShow: 2
+//                 }
+//             },
+//             {
+//                 breakpoint: 640,
+//                 settings: {
+//                     centerMode: true,
+//                     centerPadding: '0',
+//                     prevArrow: false,
+//                     nextArrow: false,
+//                     slidesToShow: 1
+//                 }
+//             },
+//             {
+//                 breakpoint: 580,
+//                 settings: {
+//                     centerMode: true,
+//                     centerPadding: '0',
+//                     prevArrow: false,
+//                     nextArrow: false,
+//                     slidesToShow: 1
+//                 }
+//             }
+//         ]
+//       });
+// });
